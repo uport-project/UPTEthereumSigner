cfc5bee483eb10101056fe7fdd123fc8ad59ff44
josh <belljh@gmail.com>
2018-01-04 10:49:57 -0800

    sample app completed

---
+++ Example/UPTEthereumSigner/UPTSignJWTViewController.h
@@ -0,0 +1,13 @@
+//
+//  UPTSignJWTViewController.h
+//  UPTEthereumSigner_Example
+//
+//  Created by josh on 1/4/18.
+//  Copyright © 2018 josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UPTSignJWTViewController : UIViewController
+
+@end
